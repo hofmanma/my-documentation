e3733539c09519233cc38cd2bb5085f4f82b0dc9
Matthias Hofmann <matthhofma@matthiamannsmbp.speedport_w_724v_09011603_05_013>
2018-04-08 20:10:51 +0200

    LEDOnOff

--- Arduino/Sketches/simpleserver/TestClient.h
+++ Arduino/Sketches/simpleserver/TestClient.h
@@ -10,11 +10,23 @@ public:
 
   TestClient( WiFiClient forClient ) : CommandClient( forClient ) {
 
-   }
+     pinMode(3, OUTPUT);
+  }
    
   virtual bool handleCommand( String cmd ) {
 
-     return true;
+      Serial.println( "Command .. " + cmd  );
+      
+      if ( cmd == "LEDON" ) {
+
+        digitalWrite(3, LOW);
+        
+      } else if ( cmd == "LEDOFF" ) {
+
+        digitalWrite(3, HIGH);
+      } 
+      
+      return true;
   }
 };
 
