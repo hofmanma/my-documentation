08f3dce84655a758dc1a992b464ba0572d96a019
Matthias Hofmann <matthhofma@matthiamannsmbp.speedport_w_724v_09011603_05_013>
2018-04-08 09:35:57 +0200

    DHT22

--- Arduino/Sketches/DHT22.ino
+++ Arduino/Sketches/DHT22.ino
@@ -5,23 +5,19 @@
 #define DHTPIN 8                                           
 #define DHTTYPE DHT22                                      
 DHT dht(DHTPIN, DHTTYPE);                                  
- 
-float humidity, temperature;                               
- 
+  
 void setup() {
   dht.begin();   
   Serial.begin(9600);                                            
 }
  
 void loop() {
-  humidity = dht.readHumidity();                           
-  temperature = dht.readTemperature();                     
 
   Serial.println( "Temperature:" );
-  Serial.println( temperature );
+  Serial.println( dht.readTemperature() );
 
   Serial.println( "Humidity:" );
-  Serial.println( humidity );
+  Serial.println( dht.readHumidity() );
  
   delay(2000);
 }
